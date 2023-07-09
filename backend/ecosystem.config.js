1270bf2669dc960622e676be6a9d2ff2044586d7
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-07-10 00:48:41 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
+} = process.env;
+
 module.exports = {
-  apps : [{
-    name   : "mesto-api",
+  apps: [{
+    name: "mesto-backend",
     script: './dist/app.js',
   }],
   deploy: {
@@ -8,10 +14,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: DEPLOY_REPO,
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend &&  pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
