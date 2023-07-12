6e48f899c030de20c3683a285773e52c18bcd897
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-07-12 22:07:05 +0300

    7

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd backend &&  pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,18 +6,16 @@ const {
 
 module.exports = {
   apps: [{
-    name: "mesto-backend",
-    script: './dist/app.js',
-  }],
-  deploy: {
+    name: 'mesto',
+    }],
+    deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd backend &&  pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
 }
