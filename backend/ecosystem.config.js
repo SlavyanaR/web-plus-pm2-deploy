29eb29670809a0ab1637641440f25c317a177c02
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-07-17 23:10:37 +0300

    2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend/dist && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 }
